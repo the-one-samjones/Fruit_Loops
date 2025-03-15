61e510c62e1ef3d3ef26bebc559ce9a2d89a0c3c
the-one-samjones <theonesamjones@gmail.com>
2025-03-14 21:56:16 -0500

    Added_Comments

--- Fruit_Loops.cpp
+++ Fruit_Loops.cpp
@@ -2,29 +2,29 @@
 #include <string>
 using namespace std;
 
-string fruits[5] = {"Apple", "Banana", "Cherry", "Grape", "Orange"};
+string fruits[5] = {"Apple", "Banana", "Cherry", "Grape", "Orange"};    //Declare and initialize array of original fruits
 int userIn;
 
 int main () {
-    cout << "List of Fruits: " << endl;
+    cout << "List of Fruits: " << endl;       //Display original fruits 
     for (int i = 0; i < 5; ++i) {
         cout << fruits[i] << endl;
     }
-    cout << "Number of fruits to add:  ";
+    cout << "Number of fruits to add:  ";               //Prompt user for the number of new fruits to add
     cin >> userIn;
-    if (userIn > 0) {
-        string fruitName;
-        string addFruits[userIn];
-        for (int i = 0; i < userIn; ++i) {
+    if (userIn > 0) {                                   //If user is adding any more than 0 fruits
+        string fruitName;                            //Declare fruitName variable 
+        string addFruits[userIn];                   //Declare array of new fruits based on the number to add
+        for (int i = 0; i < userIn; ++i) {                  //Loop to add new fruits to newFruits array
             cout << "Enter fruit name: ";
             cin >> fruitName;
             addFruits[i] = fruitName;
-            cout << "Fruit added: " << addFruits[i] << endl << endl;
+            cout << "Fruit added: " << addFruits[i] << endl << endl;        //Inform user that the fruit was added 
         }
-        for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 5; ++i) {            //Display original fruits   
             cout << fruits[i] << endl;
         }
-        for (int i = 0; i < userIn; ++i) {
+        for (int i = 0; i < userIn; ++i) {              //Display new fruits
             cout << addFruits[i] << endl;
         }
     }
